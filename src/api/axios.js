fc6cf8b3fd98d196b58d330056efe1abd7df595c
Original Timi <timileyinoyelekan11@gmail.com>
2024-03-13 13:56:45 +0100

    Update API base URL and add base path for admin routes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const  axiosInstance = axios.create({
-    baseURL:"https://jospat-api.onrender.com",
+    baseURL:"http://localhost:5000",
     // headers:{
     //     Authorization:`Bearer ${typeof window != "undefined" &&window.sessionStorage.getItem("sid")}`
     // }

16a03aa7efd4fc4f77e7df6da07c47d398af7159
Original Timi <timileyinoyelekan11@gmail.com>
2024-06-29 15:01:18 -0700

    feat: change api URL

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://us-central1-jospat-ng.cloudfunctions.net/api/v1",
+  // baseURL: "https://us-central1-jospat-ng.cloudfunctions.net/api/v1",
+  baseURL: "https://jospat.koyeb.app/v1",
   headers: {
-    Accept:"*/*",
+    Accept: "*/*",
     Authorization: `Bearer ${typeof window != "undefined" ? window.sessionStorage.getItem("authenticated") : "none"}`,
   },
 });

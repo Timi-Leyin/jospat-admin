799f9b4cbd4f044da08f5bee985c5430706459cb
Timileyin Oyelekan <timileyinoyelekan11@gmail.com>
2024-09-29 13:24:53 +0100

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "https://us-central1-jospat-ng.cloudfunctions.net/api/v1",
-  baseURL: "https://jospat-api.koyeb.app/v1",
+  baseURL: "https://unemployed-harriot-original-timi-6e442e4d.koyeb.app/v1",
   headers: {
     Accept: "*/*",
     Authorization: `Bearer ${typeof window != "undefined" ? window.sessionStorage.getItem("authenticated") : "none"}`,
